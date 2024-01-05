f6a345e4ae1b0fb0e88b4350cacc415a268f3d50
ianodad <ianodad@gmail.com>
2024-01-05 08:10:30 +0300

    working on navbar and responsive

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+
+interface Props {
+  openNav: () => void;
+}
+export const Navbar = ({ openNav }: Props) => {
+  return (
+    <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
+      <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
+        <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
+          IAN
+          <span className="text-yellow-300">ADERA</span>
+        </h1>
+        <div className="nav-link">Home</div>
+        <div className="nav-link">About</div>
+        <div className="nav-link">Projects</div>
+        <div className="nav-link">Skills</div>
+        <div className="nav-link">Experience</div>
+        <div className="nav-link">Contact</div>
+        <Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-gray-300" />
+      </div>
+    </div>
+  );
+};
